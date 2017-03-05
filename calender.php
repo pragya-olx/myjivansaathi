453edbd1fd28ec221526142b6e7775f82cea193f
Pragya Gupta <pragya.gupta@olx.com>
2017-03-05 22:49:04 +0530

    fxes

--- calender.php
+++ calender.php
@@ -5,7 +5,7 @@ include('menu.php');?>
 
 <h3 style="margin: 12px 151px;background: beige;"> Build your event calender. Mark the important dates! </h3>
     <link type="text/css" rel="stylesheet" href="assets/style.css"/>
-<link type="text/css" rel="stylesheet" href="assets/">
+<link type="text/css" rel="stylesheet" href="/assets/add-new-event.png">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 
 
