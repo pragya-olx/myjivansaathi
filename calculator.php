f049596e8be62346018032df4e3be667fcdbb632
Pragya Gupta <pragya.gupta@olx.com>
2017-03-10 11:33:34 +0530

    fixes

--- calculator.php
+++ calculator.php
@@ -1,11 +1,63 @@
-<?php
-require_once 'vendor/autoload.php';
-require_once 'lib/Unirest.php';
+<?php include_once('header.php');
+    include_once('menu.php')?>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 
-// These code snippets use an open-source library. http://unirest.io/php
-$response = Unirest::get("https://love-calculator.p.mashape.com/getPercentage?fname=John&sname=Alice",
-    array(
-        "X-Mashape-Key" => "R0T97HICtFmshYZ5DlHCLQgiRLkap1sq7Jpjsnon285UTL41XE",
-        "Accept" => "application/json"
-    )
-);
+<body>
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-sm-6" style="background-color:lavender;">
+            FirstName 1 :<input type="text" id="firstname1">
+        </div>
+        <div class="col-sm-6" style="background-color:lavenderblush;">
+            LastName 1 :<input type="text" id="lastname1">
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-6" style="background-color:lavender;">
+            FirstName 2 :<input type="text" id="firstname2">
+        </div>
+        <div class="col-sm-6" style="background-color:lavenderblush;">
+            LastName 2 : <input type="text" id="lastname2">
+        </div>
+    </div>
+
+</div>
+<input type="submit" onclick="submitData();" class="btn btn-success" style="text-align: center;margin-left:30%;margin-right: 30%; width:40%"></input>
+<div class="row">
+    <div id="result" style="text-align: center"> </div>
+</div>
+
+
+<script type="text/javascript">
+   function submitData(){
+       var data = {
+           firstname1 : $("#firstname1").val(),
+           lastname1 : $("#lastname1").val(),
+           firstname2 : $("#firstname2").val(),
+           lastname2 : $("#lastname2").val()
+       };
+
+       $.ajax({
+           data: data,
+           type: 'post',
+           success: function(result) {
+               alert("woah ! Your Love percentage is ready"); //this alert is fired
+               result = (sumchars(data.firstname1)+ sumchars(data.lastname1) +  sumchars(data.firstname2)-sumchars(data.lastname2)*100)/100
+               $('div#result').text(result + '%' );
+           }
+       });
+   }
+
+   function c2n (s, i) {
+       return parseInt(s.charAt(i), 36) - 9;
+   }
+
+   function sumchars (s) {
+       var i = s.length, r = 0;
+       while (--i >= 0) r += c2n(s, i);
+       return r;
+   }
+    </script>
+</body>
+</html>
