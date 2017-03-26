6426c5560fdd33ad78a1ac076c89b400493e277b
Pragya Gupta <pragya.gupta@olx.com>
2017-03-26 22:18:43 +0530

    new pages

--- spreadsheet.php
+++ spreadsheet.php
@@ -3,5 +3,5 @@ include('menu.php');
 ?>
 
 <script src="dhtmlxSpreadsheet_v21/codebase/spreadsheet.php?sheet=2&key=key2&parent=box" ></script>
-<div id="box" style="width: 800px; height: 400px; background-color:white;"></div>
+<div id="box" style="width: 1200px; height: 800px; background-color:white;"></div>
 
