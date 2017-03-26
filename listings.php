86ae1567098ee24f414d317e521ee528d96bd25c
Pragya Gupta <pragya.gupta@olx.com>
2017-03-26 22:17:41 +0530

    new pages

--- listings.php
+++ listings.php
@@ -10,12 +10,12 @@ while ($row = mysql_fetch_array($query, MYSQL_BOTH)) {
 ?>
     <div style="clear: both;">
     <a href="/detail.php?id=<?php echo $row['id'];?>">
-        <div style ="border:1px solid red; padding: 5px; margin : 5px;float: left; background-color: lightgray; height:100px; width:100px;">
+        <div style ="padding: 5px; margin : 5px;float: left; background-color: lightgreen; height:100px; width:100px;">
             <p> <img style="height: 80px;" src="assets/girl.png"></img></p>
         </div>
 
 
-        <div  style ="border: 1px solid blue; padding: 5px; float:right; margin: 5px;background-color: lightgray; height:100px;width: 730px;"">
+        <div  style ="padding: 5px; float:right; margin: 5px;background-color: lightyellow; height:100px;width: 1157px;"">
             <span> Name: <?php echo $row['name'];?></span><br>
              <span> Date Of Birth: <?php echo $row['date_of_birth'];?></span><br>
               <span> Mobile No: <?php echo $row['mobile_no'];?></span><br>
